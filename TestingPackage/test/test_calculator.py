8545755d17faf196d501c2bceb63155a4c146dff
alexarirok <akorir233@gmail.com>
2019-06-06 13:32:12 +0300

    Static folders

---
+++ TestingPackage/test/test_calculator.py
@@ -0,0 +1,23 @@
+from app.calculator import Calculator
+import unittest
+
+class TddInPythonExample(unittest.TestCase):
+
+    def setUp(self): #setup() method allows us to put things in place before each test case.
+        self.calc = Calculator()
+
+    def test_calculator_add_method_returns_correct_result(self):
+        result = self.calc.add(2, 2)
+        self.assertEqual(4, result)
+
+    def test_calculator_returns_error_message_if_both_args_not_numbers(self):
+        self.assertRaises(ValueError, self.calc.add, 'two', 'three')
+
+    def test_calculator_returns_error_message_if_x_arg_not_number(self):
+        self.assertRaises(ValueError, self.calc.add, "two", 3)
+
+    def test_calculator_returns_error_message_if_y_arg_not_number(self):
+        self.assertRaises(ValueError, self.calc.add, "2", 3)
+
+if __name__ == "__main__":
+    unittest.main()
